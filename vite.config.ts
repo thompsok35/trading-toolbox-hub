979b1aeb5434e54e40413bbbccdf11667cf977c2
Keith Thompson <thompsok35@gmail.com>
2026-03-21 23:01:50 -0400

    Fix: Configure vite.config.ts to natively bind port and host for railway proxy

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,13 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  preview: {
+    host: true,
+    port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
+    allowedHosts: true
+  },
+  server: {
+    host: true,
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5173
+  }
 })
